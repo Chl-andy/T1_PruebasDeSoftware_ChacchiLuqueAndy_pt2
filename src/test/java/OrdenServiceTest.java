eb46cb9172f8b8de0c6d3ce9bf62292578025aa7
Chl_andy <realforever.info@gmail.com>
2026-03-30 23:53:42 -0500

    Segundo commit - Test 1: "Descuento si el total es mayor a 500"

--- src/test/java/OrdenServiceTest.java
+++ src/test/java/OrdenServiceTest.java
@@ -7,25 +7,25 @@ import static org.mockito.Mockito.*;
 
 public class OrdenServiceTest {
 
+
     @Test
-    void testCancelarOrdenPorStock() {
+    void testDescuentoSiTotalMayor500() {
         StockService stockService = mock(StockService.class);
         ClienteService clienteService = mock(ClienteService.class);
 
-        Producto p1 = new Producto("Laptop", 300, 0, 1); // sin stock
-        Producto p2 = new Producto("Mouse", 50, 10, 1);
+        Producto p1 = new Producto("Monitor", 400, 10, 1);
+        Producto p2 = new Producto("Teclado", 150, 10, 1);
 
         Orden orden = new Orden();
         orden.setProductos(Arrays.asList(p1, p2));
-        orden.setCliente(new Cliente("Carlos", true));
+        orden.setCliente(new Cliente("Ana", true));
 
-        when(stockService.tieneStock(p1)).thenReturn(false);
-        when(stockService.tieneStock(p2)).thenReturn(true);
+        when(stockService.tieneStock(any())).thenReturn(true);
         when(clienteService.clienteActivo(orden.getCliente())).thenReturn(true);
 
         OrdenService service = new OrdenService(stockService, clienteService);
         String resultado = service.registrarOrden(orden);
 
-        assertEquals("Orden cancelada por falta de stock", resultado);
+        assertEquals("Orden registrada con descuento 10%", resultado);
     }
 }
